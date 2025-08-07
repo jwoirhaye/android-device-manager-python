2d4de1cdbe9e725ae04f6f56652b10a99063679a
Jérémy Woirhaye <jerem.woirhaye@gmail.com>
2025-08-07 12:06:52 +0200

    feat(android-device): add start and stop methods to control emulator lifecycle
    - start(): boots the emulator, waits for sys.boot_completed, updates internal state
    - stop(): kills the emulator via ADB and cleans up the emulator process
    - integrates AdbClient and EmulatorManager to coordinate startup and shutdown

--- examples/create_device.py
+++ examples/create_device.py
@@ -1,17 +1,25 @@
+import logging
+
 from android_device_manager import AndroidDevice
 from android_device_manager.avd.config import AVDConfiguration
+from android_device_manager.emulator.config import EmulatorConfiguration
+
+logging.basicConfig(level=logging.INFO)
+
+
+avd_configuration = AVDConfiguration(
+        name="test_from_lib", package="system-images;android-36;google_apis;x86_64"
+    )
+
+emulator_configuration = EmulatorConfiguration()
 
 device = AndroidDevice(
-    AVDConfiguration(
-        name="test_from_lib",
-        package="system-images;android-36;google_apis;x86_64"
-    )
+    avd_config=avd_configuration,
+    emulator_config=emulator_configuration,
 )
 
 device.create()
-
-print(device._avd_manager.list())
-
+device.start()
+device.stop()
 device.delete()
 
-print(device._avd_manager.list())
